d993d37985dd9a3a4c44ec5b3bb624a0b5f2ead1
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-09-18 17:47:26 +0200

    refactor: clean up profiling code

--- profiling/profile_cbrt.py
+++ profiling/profile_cbrt.py
@@ -7,24 +7,18 @@ from hypothesis import strategies as st
 from vyper.utils import SizeLimits
 
 PROFILING_SETTINGS = dict(
-    max_examples=50000,
+    max_examples=1000000,
     deadline=timedelta(seconds=1000),
 )
 PROFILING_OUTPUT = "./profiling/data/profile_cbrt.csv"
 
-with open(PROFILING_OUTPUT, "w") as f:
-    f.write(
-        "input,initial_value,cbrt_ideal,cbrt_implementation_noguess,"
-        "cbrt_implementation_guess,gas_used_noguess,gas_used_initial_value\n"
-    )
-
 
 @given(
     val=st.integers(min_value=0, max_value=SizeLimits.MAX_UINT256 / 10**18),
-    guess_range=st.floats(min_value=0.1, max_value=1.9),
+    guess_range=st.floats(min_value=0.01, max_value=3),
 )
 @settings(**PROFILING_SETTINGS)
-def profile_call(tricrypto_math, output_file, val, guess_range):
+def profile_call(tricrypto_math, profiling_output, val, guess_range):
 
     cbrt_ideal = int((val / 10**18) ** (1 / 3) * 10**18)
     initial_value = random.randint(
@@ -39,10 +33,14 @@ def profile_call(tricrypto_math, output_file, val, guess_range):
         cbrt_guess = tricrypto_math.eval(f"self.cbrt({val}, {initial_value})")
         gasused_cbrt_guess = tricrypto_math._computation.get_gas_used()
 
-        output_file.write(
+        profiling_data = (
             f"{val},{initial_value},{cbrt_ideal},{cbrt_noguess},"
             f"{cbrt_guess},{gasused_cbrt_noguess},{gasused_cbrt_guess}\n"
         )
+        if profiling_data not in profiling_output:
+            profiling_output.append(profiling_data)
+        else:
+            assume(False)  # if data already exists, assume False example
 
     # we don't care about contract errors here, so in case of reversions just
     # assume that the example generated was bad
@@ -56,6 +54,16 @@ if __name__ == "__main__":
     with boa.env.prank(deployer_addr):
         tricrypto_math = boa.load("contracts/CurveCryptoMathOptimized3.vy")
 
-    output_file = open(PROFILING_OUTPUT, "a")
-    profile_call(tricrypto_math, output_file)
-    output_file.close()
+    global profiling_output
+    profiling_output = []
+    profile_call(tricrypto_math, profiling_output)
+
+    # TODO: output is none for some reason. make sure something is returned.
+    with open(PROFILING_OUTPUT, "w") as f:
+        f.write(
+            "input,initial_value,cbrt_ideal,cbrt_implementation_noguess,"
+            "cbrt_implementation_guess,gas_used_noguess,"
+            "gas_used_initial_value\n"
+        )
+        for profile in profiling_output:
+            f.write(profile)
